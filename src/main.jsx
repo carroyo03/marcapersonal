ed22d80c47cf215690880b1be7e699ae327a5860
carroyo03 <cgarroyolorenzo@gmail.com>
2024-11-24 11:13:52 +0100

    loading changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { lazy, Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const App = lazy(() => import('./App.jsx'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const GlobalErrorHandler = ({ children }) => {
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      {children}
+    </React.Suspense>
+  );
+};
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+    <GlobalErrorHandler>
+      <App />
+      </GlobalErrorHandler>
+    </Suspense>
   </React.StrictMode>
-)
+);
