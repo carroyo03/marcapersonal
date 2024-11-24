c755801153e04f0c9f04e175cb28602fa33e63be
carroyo03 <cgarroyolorenzo@gmail.com>
2024-11-24 15:51:15 +0000

    accesibility changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
     return (
         <div className="App">
             <div className='profile-wrapper'>
-                <img id="profpic" src="assets/carlos.jpg" alt="Profile Picture"/>
+                <img id="profpic" src="assets/carlos.jpg" alt="Profile Picture" loading='lazy'/>
             </div>
             <h1>Carlos Arroyo</h1>
             <h2>Full-Stack Developer & AI Engineer</h2>
--- src/Links.jsx
+++ src/Links.jsx
@@ -7,17 +7,17 @@ export default function Links() {
             <h2>Follow Me</h2>
             <div className="divider">
                 <a href="https://github.com/carroyo03" target="_blank" rel="noreferrer">
-                    <button id="GitHub" className="btn btn-primary rounded-2 border border-2">
+                    <button aria-label="GitHub" id="GitHub" className="btn btn-primary rounded-2 border border-2">
                         <Github />
                     </button>
                 </a>
                 <a href="https://www.linkedin.com/in/carlosarroyolorenzo" target="_blank" rel="noreferrer">
-                    <button id="LinkedIn" className="btn btn-primary rounded-2 border border-2">
+                    <button aria-label="LinkedIn" id="LinkedIn" className="btn btn-primary rounded-2 border border-2">
                         <Linkedin />
                     </button>
                 </a>
                 <a href="mailto:cgarroyolorenzo@gmail.com" target="_blank" rel="noreferrer">
-                    <button id="Mail" className="btn btn-primary rounded-2 border border-2">
+                    <button aria-label="Mail" id="Mail" className="btn btn-primary rounded-2 border border-2">
                         <Mail /> 
                     </button>
                 </a>
