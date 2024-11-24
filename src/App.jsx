5b4875b4944c7131d3cc686201edd436c0375baf
carroyo03 <cgarroyolorenzo@gmail.com>
2024-11-24 10:02:01 +0000

    create app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+export default function App() {
+    return (
+        <div className="App">
+            <img id="profpic" src="assets/carlos.jpg" alt="Profile Picture" />
+            <h1>Carlos Arroyo</h1>
+            <h2>Full-Stack Developer & AI Engineer</h2>
+            <section>
+                <h3>About Me</h3>
+                <p>
+                    I am a full-stack developer and AI engineer with a passion for technology and innovation. I have experience in web development, machine learning, and cloud computing. I am always looking for new opportunities to learn and grow as a professional.
+                </p>
+            </section>
+        </div>
+    )
+};
